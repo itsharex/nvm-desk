08173c0d0ca27241d7e4f383a76635286c4f6826
kdydesign <kdydesign@gmail.com>
2023-12-04 10:38:14 +0900

    chore: add quasar

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
+import {quasar, transformAssetUrls} from '@quasar/vite-plugin';
 
 export default defineConfig(async () => ({
-  plugins: [vue()],
-  clearScreen: false,
-  server: {
-    port: 1420,
-    strictPort: true,
-  }
+    plugins: [
+        vue({
+            template: {transformAssetUrls}
+        }),
+        quasar({
+            sassVariables: 'src/assets/style/quasar-variable.sass'
+        })
+    ],
+    clearScreen: false,
+    server: {
+        port: 1420,
+        strictPort: true,
+    }
 }));
